9e3ccbbe28d5239349ff89c46db0122f66a33e92
galens <260827400@qq.com>
2021-07-24 12:29:36 +0800

    添加查看历史功能

---
+++ Models/ShortcutsHistory.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwTools.PowerShortcut.Models
+{
+    class ShortcutsHistory
+    {
+        private int _limit = 20;
+        public List<string> History { get; set; } = new List<string>();
+
+        public void Push(string shortcutName) {
+            if (string.IsNullOrEmpty(shortcutName)) return;
+
+            if (History.Count == _limit)
+            {
+                History.RemoveAt(0);
+            }
+            History.Add(shortcutName);
+            _currentIndex = History.Count - 1;
+        }
+
+        private int _currentIndex = 0;
+
+        public string Previous()
+        {
+            if (History.Count < 1) return string.Empty;
+
+            _currentIndex -= 1;
+            if (_currentIndex < 0)
+            {
+                _currentIndex += History.Count;
+            }
+
+            return History[_currentIndex];
+        }
+
+        public string Next()
+        {
+            if (History.Count < 1) return string.Empty;
+
+            _currentIndex += 1;
+            if (_currentIndex >= History.Count)
+            {
+                _currentIndex -= History.Count;
+            }
+
+            return History[_currentIndex];
+        }
+    }
+}
--- Views/FloatWindow.xaml.cs
+++ Views/FloatWindow.xaml.cs
@@ -8,6 +8,7 @@ using System.Runtime.InteropServices;
 using System.Windows;
 
 using Bentley.MstnPlatformNET.WPF;
+using SwTools.PowerShortcut.Models;
 using SwTools.PowerShortcut.ViewModels;
 
 namespace SwTools.PowerShortcut.Views
@@ -42,24 +43,41 @@ namespace SwTools.PowerShortcut.Views
             CommandText.KeyUp += CommandText_KeyUp;
         }
 
+        private ShortcutsHistory _history = new ShortcutsHistory();
         private void CommandText_KeyUp(object sender, System.Windows.Input.KeyEventArgs e)
         {
             // 打开快捷
             if (e.Key == System.Windows.Input.Key.Space || e.Key == System.Windows.Input.Key.Enter)
             {
                 // 打开命令
-
                 // 按了确认键
                 Bentley.MstnPlatformNET.Session.Instance.Keyin(_viewModel.Shortcut.Keyin);
+
+                // 保存快捷键
+                _history.Push(_viewModel.Shortcut.Name);
+
+                // 重置为空
                 _viewModel.Name = string.Empty;
 
                 // 失去焦点
                 Helper.Win32Helper.SetForegroundWindow(m_wndHelper.MSWinIntPtr);
             }
-            else if(e.Key == System.Windows.Input.Key.Tab)
+            else if (e.Key == System.Windows.Input.Key.Tab)
             {
                 // 自动补全
                 _viewModel.Name = Models.ShortcutConfig.Instance.TabFullName(_viewModel.Name);
+                // 设置光标到末尾
+                CommandText.Select(_viewModel.Name.Length, 0);
+            }
+            else if (e.Key == System.Windows.Input.Key.Up)
+            {
+                _viewModel.Name = _history.Previous();
+                // 设置光标到末尾
+                CommandText.Select(_viewModel.Name.Length, 0);
+            }
+            else if (e.Key == System.Windows.Input.Key.Down)
+            {
+                _viewModel.Name = _history.Next();
                 // 设置光标到末尾
                 CommandText.Select(_viewModel.Name.Length, 0);
             }
