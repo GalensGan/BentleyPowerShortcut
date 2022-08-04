d067d115d13d50b9e9a49827a495029b51e8f563
GALENS <260827400@qq.com>
2022-08-04 20:36:53 +0800

    relase: 修改版本号

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -55,5 +55,5 @@ using System.Windows;
 //      Build Number
 //      Revision
 //
-//[assembly: AssemblyVersion ("1.0.0.0")]
-//[assembly: AssemblyFileVersion ("1.0.0.0")]
+[assembly: AssemblyVersion ("1.0.2.0")]
+// [assembly: AssemblyFileVersion ("1.0.0.0")]
